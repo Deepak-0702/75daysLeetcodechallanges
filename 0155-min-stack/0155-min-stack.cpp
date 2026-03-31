a367ff66c01eedc9d678ab53149752673d4e7e88
Deepak-0702 <deepakverma010105@gmail.com>
2026-03-31 22:02:43 +0530

    Time: 1 ms (73.41%), Space: 23.3 MB (83.83%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,31 +1,30 @@
 class MinStack {
 public:
-stack<int>s;
-stack<int>minstack;
+stack<pair<int,int>>s;
     MinStack() {
         
     }
     
     void push(int val) {
-        s.push(val);
-        if(minstack.empty() || val<=minstack.top()){
-            minstack.push(val);
+        if(s.empty()){
+            s.push({val,val});
+        }
+        else{
+            int mini=min(val,s.top().second);
+            s.push({val,mini});
         }
     }
     
     void pop() {
-        if(s.top()==minstack.top()){
-            minstack.pop();
-        }
         s.pop();
     }
     
     int top() {
-        return s.top();
+        return s.top().first;
     }
     
     int getMin() {
-        return minstack.top();
+        return s.top().second;
     }
 };
 
