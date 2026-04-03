8f6fc3e9bc0fd3f84ae265f483465e8faf020969
Deepak-0702 <deepakverma010105@gmail.com>
2026-04-04 00:33:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (37.11%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-const int MOD=1e9+7;
-int helper(int n,vector<int>&dp){
-    if(n==0 || n==1) return 1;
-    if(n==2) return 2;
-    if(dp[n]!=-1) return dp[n];
-    long long left=helper(n-1,dp);
-    // ye wala case ki jarurat nhi hai kyu ki vo int right=helper(n-2);
-    long long trim=helper(n-3,dp);
-    return dp[n]= (2*left%MOD+trim%MOD)%MOD;
-}
     int numTilings(int n) {
-        vector<int>dp(n+1,-1);
-        return helper(n,dp);
+        const int MOD=1e9+7;
+        vector<long long>dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        if(n>=2) dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=(2*dp[i-1]%MOD+dp[i-3]%MOD)%MOD;
+        }
+        return dp[n];
     }
 };
