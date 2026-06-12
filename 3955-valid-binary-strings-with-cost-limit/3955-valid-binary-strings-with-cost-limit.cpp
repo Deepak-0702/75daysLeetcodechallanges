2e710d88dd6e957c26cd6f746a72cfacc57ab652
Deepak-0702 <deepakverma010105@gmail.com>
2026-06-12 13:53:44 +0530

    Time: 8 ms (90.52%), Space: 30 MB (82.84%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.cpp
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    void dfs(int idx, int n, int cost, int k,
-             string &curr, vector<string>& ans) {
-
-        if(cost > k) return;
-
-        if(idx == n){
-            ans.push_back(curr);
+  
+    void helper(string &temp,int n,int i,int cost ,int k,vector<string>&ans){
+        if(cost>k) return;
+        if(i==n){
+            ans.push_back(temp);
             return;
         }
-
-        // place 0
-        curr.push_back('0');
-        dfs(idx + 1, n, cost, k, curr, ans);
-        curr.pop_back();
-
-        // place 1 only if previous wasn't 1
-        if(idx == 0 || curr.back() != '1'){
-            curr.push_back('1');
-            dfs(idx + 1, n, cost + idx, k, curr, ans);
-            curr.pop_back();
+        temp.push_back('0');
+        helper(temp,n,i+1,cost,k,ans);
+        temp.pop_back();
+        if(i==0 || temp.back()!='1'){
+            temp.push_back('1');
+            helper(temp,n,i+1,cost+i,k,ans);
+            temp.pop_back();
         }
+        
     }
-
     vector<string> generateValidStrings(int n, int k) {
-        vector<string> ans;
-        string curr;
-
-        dfs(0, n, 0, k, curr, ans);
-
+        string temp;
+        vector<string>ans;
+        helper(temp,n,0,0,k,ans);
         return ans;
     }
 };
