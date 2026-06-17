702f87a593f6fbb3b623056058f2fe0629b97f28
Deepak-0702 <deepakverma010105@gmail.com>
2026-06-17 19:18:38 +0530

    Time: 33 ms (40.23%), Space: 24.1 MB (28.74%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -2,37 +2,42 @@ class Solution {
 public:
     char processStr(string s, long long k) {
         long long len=0;
-        for(char c:s){
-            if(c=='*'){
-                len=max(0LL,len-1);
+        for(char ch:s){
+            if(islower(ch)) len++;
+            else if(ch=='*'){
+                if(len>0){
+                    len--;
+                }
             }
-            else if(c=='#'){
+            else if(ch=='#')
+            {
                 len*=2;
-            }
-            else if(c!='%'){
-                len++;
             }
         }
         if(k>=len) return '.';
-        for(int i=s.size()-1;i>=0;i--){
-            char c=s[i];
-            if(c=='*'){
-                len++;
-            }
-            else if(c=='#'){
-                len/=2;
-                if(k>=len) k-=len;
-            }
-            else if(c=='%'){
-                k=len-1-k;
-            }
-            else{
-                len--;
-                if(k==len){
-                    return c;
+        for(int i=s.size()-1;i>=0;i--)
+            {
+                char ch=s[i];
+                if(islower(ch))
+                {
+                    if(len-1==k) return ch;
+                    len--;
+                }
+                else if(ch=='#')
+                {
+                    len/=2;
+                    if(k>=len){
+                        k-=len;
+                    }
+                }
+                else if(ch=='*')
+                {
+                    len++;
+                }
+                else if(ch=='%'){
+                    k=len-1-k;
                 }
             }
-        }
         return '.';
     }
 };
