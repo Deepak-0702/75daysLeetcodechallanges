987eccfe4d6bddcb081de23854ef72df7cdeb3ee
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-07 21:29:27 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+int helper(int idx,vector<int>&nums,vector<int>&dp){
+    if(idx>=nums.size()) return 0;
+    if(idx==nums.size()){
+        return nums[idx];
+    }
+    if(dp[idx]!=-1) return dp[idx];
+    int rob=nums[idx]+helper(idx+2,nums,dp);
+    int skip=helper(idx+1,nums,dp);
+    return dp[idx]=max(rob,skip);
+}
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==1){
-            return nums[0];
-        }
-        if(n==2) return max(nums[0],nums[1]);
-        vector<int>dp(n+1,0);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            int rob=nums[i]+dp[i-2];
-            int skip=dp[i-1];
-            dp[i]=max(rob,skip);
-        }
-        return dp[n-1];
+        vector<int>dp(n+1,-1);
+        return helper(0,nums,dp);
     }
 };
