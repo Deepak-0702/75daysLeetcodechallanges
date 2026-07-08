2a912768c61329ca92975998b28a0b3770874503
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-08 14:09:35 +0530

    Time: 0 ms (100%), Space: 10.9 MB (37.13%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-int helper(vector<int>&nums,int idx,int end,vector<int>&dp){
+int helper(int idx,int end,vector<int>&nums,vector<int>&dp){
     if(idx>end) return 0;
-    if(dp[idx]!=-1){
-        return dp[idx];
-    }
-    int rob=nums[idx]+helper(nums,idx+2,end,dp);
-    int skip=helper(nums,idx+1,end,dp);
-    return dp[idx]=max(rob,skip);
+    if(dp[idx]!=-1) return dp[idx];
+    int rob=nums[idx]+helper(idx+2,end,nums,dp);
+    int skip=helper(idx+1,end,nums,dp);
+    return dp[idx]= max(rob,skip);
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
         vector<int>dp1(n,-1);
-        int case1=helper(nums,0,n-2,dp1);
+        int case1=helper(0,n-2,nums,dp1);
         vector<int>dp2(n,-1);
-        int case2=helper(nums,1,n-1,dp2);
+        int case2=helper(1,n-1,nums,dp2);
         return max(case1,case2);
     }
 };
