3d4b1a634ec5204b44a2f6e31c6b0058b09baa3b
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-08 16:55:06 +0530

    Time: 124 ms (24.38%), Space: 14.6 MB (22.54%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -6,6 +6,7 @@ int helper(int n,vector<int>&prfsq,vector<int>&dp){
     if(dp[n]!=-1) return dp[n];
     int mini=INT_MAX;
     for(int i=0;i<prfsq.size();i++){
+        if(prfsq[i]>n) break;
         int ans=helper(n-prfsq[i],prfsq,dp);
         if(ans!=INT_MAX){
             mini=min(mini,1+ans);
