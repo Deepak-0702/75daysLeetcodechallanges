4ef508b5b2f60f0fdbac2f2443b6a817d898c404
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-16 10:37:40 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
+        if(s.size()!=t.size()) return false;
+        //anagram means having same character with having each same freq 
+        //and length of both string is same
         vector<int>freq(26,0);
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.size();i++){
             freq[s[i]-'a']++;
             freq[t[i]-'a']--;
         }
         for(int i=0;i<freq.size();i++){
-            if(freq[i]!=0) return false;
+            if(freq[i]!=0){
+                return false;
+            }
         }
         return true;
     }
