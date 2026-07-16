1ae839d3abba2a2bc3a3d56b46090bb4f2628133
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-16 16:23:40 +0530

    Time: 4 ms (52.68%), Space: 10.1 MB (30.8%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        if(s1.size()>s2.size()) return false;
         vector<int>freq1(26,0);
         vector<int>freq2(26,0);
         for(int i=0;i<s1.size();i++){
