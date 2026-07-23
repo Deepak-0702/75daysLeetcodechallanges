3a7672985e8308fafd97b8c9e774bd0a64899efb
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-24 01:07:30 +0530

    Time: 12 ms (41.51%), Space: 25 MB (27.45%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-int helper(int idx,int time,vector<int>&sat,vector<vector<int>>&dp){
-    if(idx==sat.size()) return 0;
-    
-    if(dp[idx][time]!=-1) return dp[idx][time];
-    int inc=sat[idx]*(time+1)+helper(idx+1,time+1,sat,dp);
-    int excl=0+helper(idx+1,time,sat,dp);
 
-    return dp[idx][time]=max(inc,excl);
-
-}
-    int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(),satisfaction.end());
-        int n=satisfaction.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return helper(0,0,satisfaction,dp);
+    int maxSatisfaction(vector<int>& sat) {
+        sort(sat.begin(),sat.end());
+        int n=sat.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int idx=n-1;idx>=0;idx--){
+            for(int time=idx;time>=0;time--){
+                int inc=sat[idx]*(time+1)+dp[idx+1][time+1];
+                int excl=dp[idx+1][time];
+                dp[idx][time]=max(inc,excl);
+            }
+        }
+        return dp[0][0];
     }
 };
