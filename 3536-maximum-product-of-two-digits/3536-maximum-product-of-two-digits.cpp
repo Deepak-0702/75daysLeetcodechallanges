17baec1518b768d385a07c88d0f2fc5e52dd3e11
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-25 12:38:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (74.03%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        //agr hum nested loop ko hata chahte hai tho 
-        //as we know we the product of largest and second largest element 
-        int first=0;
-        int second=0;
+        int first=-1;
+        int second=-1;
         while(n>0){
             int d=n%10;
             if(d>=first){
                 second=first;
                 first=d;
             }
-            else if(d>second){
+            else if(d>=second && first!=d){
                 second=d;
             }
-            n/=10;
+            n=n/10;
         }
         return first*second;
     }
