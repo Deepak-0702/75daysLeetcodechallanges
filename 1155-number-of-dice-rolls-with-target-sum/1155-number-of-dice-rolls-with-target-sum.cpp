a3da6f2de41f465c1cbb33cc516990e08dd9a535
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-28 17:04:18 +0530

    Time: 15 ms (46.52%), Space: 9.8 MB (82.93%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
 long long mod=1e9+7;
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<long long>>dp(n+1,vector<long long>(target+1,0));
-        //according to base case
-        dp[0][0]=1;
+        //sapce optimzation
+        vector<long long>prev(target+1,0);
+        vector<long long>curr(target+1,0);
+        prev[0]=1;
         for(int i=1;i<=n;i++){//as we know that at idx=0 we know that value already so we dont need to calculate 
             for(int j=1;j<=target;j++){
                 long long ans=0;
                 for(int z=1;z<=k;z++){
                     if(j-z>=0){
-                        ans=(ans+dp[i-1][j-z])%mod;
+                        ans=(ans+prev[j-z])%mod;
                     }
                 }
-                dp[i][j]=ans;
+                curr[j]=ans;
             }
+            prev=curr;
             
         }
-        return (int)dp[n][target];
+        return (int)prev[target];
     }
 };
