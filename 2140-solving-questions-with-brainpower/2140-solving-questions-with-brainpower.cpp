71cffec58723827e7a443f18e42b859b4628f89a
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-28 13:16:19 +0530

    Time: 17 ms (9.51%), Space: 127.6 MB (16.1%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+long long helper(int idx,vector<vector<int>>&questions,vector<long long>&dp){
+    if(idx>=questions.size()) return 0;
+
+    if(dp[idx]!=-1) return dp[idx];
+    //brainpower repersents how much index u skip
+    long long solve=questions[idx][0]+helper(idx+questions[idx][1]+1,questions,dp);
+    long long skip=helper(idx+1,questions,dp);
+
+    return dp[idx]=max(solve,skip);
+
+}
+    long long mostPoints(vector<vector<int>>& questions) {
+        vector<long long>dp(questions.size(),-1);
+        return helper(0,questions,dp);
+    }
+};
