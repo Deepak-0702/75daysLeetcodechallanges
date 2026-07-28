f06ca6cac6b2a8c8254fed2b0d4bf08c77c76398
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-28 16:47:06 +0530

    Time: 17 ms (33.36%), Space: 13.6 MB (8.43%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
 long long mod=1e9+7;
-long long helper(int n,int k,int target,vector<vector<long long>>&dp){
-    if(target<0) return 0;
-    if(n==0 && target!=0) return 0;
-    if(n==0 && target==0) return 1;
-    if(n!=0 && target==0) return 0;
-    if(dp[n][target]!=-1) return dp[n][target];
-
-    long long ans=0;
-    for(int i=1;i<=k;i++){
-        ans=(ans+helper(n-1,k,target-i,dp))%mod;
-    }
-    return dp[n][target]=ans%mod;
-
-} 
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<long long>>dp(n+1,vector<long long>(target+1,-1));
-        return (int)helper(n,k,target,dp);
+        vector<vector<long long>>dp(n+1,vector<long long>(target+1,0));
+        //according to base case
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){//as we know that at idx=0 we know that value already so we dont need to calculate 
+            for(int j=1;j<=target;j++){
+                long long ans=0;
+                for(int z=1;z<=k;z++){
+                    if(j-z>=0){
+                        ans=(ans+dp[i-1][j-z])%mod;
+                    }
+                }
+                dp[i][j]=ans;
+            }
+            
+        }
+        return (int)dp[n][target];
     }
 };
