b0a11192210ec797f1520bfe8d399de29f6138c5
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-28 11:00:16 +0530

    Time: 15 ms (32.9%), Space: 27 MB (11.36%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-int helper(int idx,int end,vector<int>&slices,int n,vector<vector<int>>&dp){
-    if(n==0 || idx>end) return 0;
-    if(dp[idx][n]!=-1) return dp[idx][n];
-    int pick=slices[idx]+helper(idx+2,end,slices,n-1,dp);
-    int notpick=helper(idx+1,end,slices,n,dp);
-
-    return dp[idx][n]=max(pick,notpick);
-}
     int maxSizeSlices(vector<int>& slices) {
         int k=slices.size();
-        vector<vector<int>>dp1(k,vector<int>(k,-1));
-        vector<vector<int>>dp2(k,vector<int>(k,-1));
-        int case1=helper(0,k-2,slices,k/3,dp1);
-        int case2=helper(1,k-1,slices,k/3,dp2);
+        //as we know there is 2 case so that there is 2 loop used here
+        //in top-down approach we used 0,k-2 then we used in tabulation from k-2,0
+        //in top-down approach we used 1,k-1 then we used in tabulation from k-1,1
+        vector<vector<int>>dp1(k+2,vector<int>(k+2,0));
+        vector<vector<int>>dp2(k+2,vector<int>(k+2,0));
+        //first case
+        for(int i=k-2;i>=0;i--){
+            for(int n=1;n<=k/3;n++){
+                int pick=slices[i]+dp1[i+2][n-1];
+                int notpick=dp1[i+1][n];
+
+                dp1[i][n]=max(pick,notpick);
+            }
+        }
+        int case1=dp1[0][k/3];
+        //second case
+        for(int i=k-1;i>=1;i--){
+            for(int n=1;n<=k/3;n++){
+                int pick=slices[i]+dp2[i+2][n-1];
+                int notpick=dp2[i+1][n];
+
+                dp2[i][n]=max(pick,notpick);
+            }
+        }
+        int case2=dp2[1][k/3];
 
         return max(case1,case2);
     }
