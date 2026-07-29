5e516ef8ab717082eccb08a183553e90e90b9385
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-29 17:35:24 +0530

    Time: 140 ms (43.47%), Space: 17.4 MB (66.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-bool helper(int idx,vector<int>&nums,int target,vector<vector<int>>&dp){
-    if(idx>=nums.size()) return false;
-    if(target<0) return false;
-    if(target==0){
-        return true;
-    }
-    if(dp[idx][target]!=-1) return dp[idx][target];
-    
-    bool inc=helper(idx+1,nums,target-nums[idx],dp);
-    
-    bool excl=helper(idx+1,nums,target,dp);
-    return dp[idx][target]=inc || excl;
-    
-}
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
@@ -25,7 +11,20 @@ bool helper(int idx,vector<int>&nums,int target,vector<vector<int>>&dp){
             return false;
         }
         int target=sum/2;
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return helper(0,nums,target,dp);
+        //tabulation 
+        vector<vector<bool>>dp(n+1,vector<bool>(target+1,false));
+        dp[n][0]=true;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=target;j++){
+                bool inc=false;
+                if(j>=nums[i]){
+                    inc=dp[i+1][j-nums[i]];    
+                }
+                bool excl=dp[i+1][j];
+                dp[i][j]=(inc||excl);
+
+            }
+        }
+        return dp[0][target];
     }
 };
