01ea0fabe0e1a31ffd4b3b422c4f5f1ad419dbca
Deepak-0702 <deepakverma010105@gmail.com>
2026-07-31 21:00:13 +0530

    Time: 18 ms (25.38%), Space: 31.9 MB (5.31%) - LeetHub

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        unordered_set<int>st;
+        int A,B;
+        int sum=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                sum+=grid[i][j];
+                if(st.find(grid[i][j])!=st.end()){
+                    A=grid[i][j];
+
+                }
+                st.insert(grid[i][j]);
+            }
+        }
+        int total=(m*n)*(m*n+1)/2;
+        B=total+A-sum;
+        return {A,B};
+    }
+};
