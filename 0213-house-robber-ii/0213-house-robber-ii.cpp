e80439a8e56c00cb682340249f5327b1e2e8a259
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-01 16:02:27 +0530

    Time: 0 ms (100%), Space: 10.3 MB (63.05%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -6,21 +6,28 @@ public:
         if(n==1) return nums[0];
 
         //tabulation
-        vector<int>dp1(n+2,0);
+        int prev2=0;
+        int prev1=0;
         for(int i=n-2;i>=0;i--){
-            int rob=nums[i]+dp1[i+2];
-            int skip=dp1[i+1];
-            dp1[i]=max(rob,skip);
+            int rob=prev2+nums[i];
+            int skip=prev1;
+            int ans=max(rob,skip);
+            prev2=prev1;
+            prev1=ans;
         }
-        int case1=dp1[0];
+        int case1=prev1;
 
-        vector<int>dp2(n+2,0);
+        prev1=0;
+        prev2=0;
+        
         for(int i=n-1;i>=1;i--){
-            int rob=nums[i]+dp2[i+2];
-            int skip=dp2[i+1];
-            dp2[i]=max(rob,skip);
+            int rob=prev2+nums[i];
+            int skip=prev1;
+            int ans=max(rob,skip);
+            prev2=prev1;
+            prev1=ans;
         }
-        int case2=dp2[1];
+        int case2=prev1;
 
         return max(case1,case2);
         
