dbaf86faa77ba1136a831f8bfe003646b19d1614
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-02 10:39:56 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.83%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,7 +10,24 @@ int helper(int i,int j,int m,int n,vector<vector<int>>&dp){
     return dp[i][j]=down+right;
 }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(0,0,m,n,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1) continue;
+                int down =0;
+                int right=0;
+                if(i+1 <m){
+                    down=dp[i+1][j];
+                }
+                if(j+1<n){
+                    right=dp[i][j+1];
+                }
+
+                dp[i][j]=down+right;
+            }
+        }
+        return dp[0][0];
     }
 };
