1f4d946ada9e071e82b8cd27a57290904773014e
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-03 16:39:16 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,7 +4,10 @@ public:
         int idx=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                swap(nums[i],nums[idx]);
+                if(i!=idx){
+                    swap(nums[i],nums[idx]);
+
+                }
                 idx++;
             }
         }
