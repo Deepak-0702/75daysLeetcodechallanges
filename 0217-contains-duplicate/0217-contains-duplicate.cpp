8e72a6d418d5dee34cb295c966d5cd9905af9e28
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-03 15:28:36 +0530

    Time: 139 ms (5.71%), Space: 113.7 MB (5.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>freq;
+        set<int>s;//find() → O(log n) ,insert() → O(log n)
         for(int num:nums){
-            freq[num]++;
-        }
-        for(auto &p:freq){
-            if(p.second>1){
+            if(s.find(num)!=s.end()){
                 return true;
             }
+            s.insert(num);
         }
         return false;
+
+        //so total time complexity:- is O(n log n)
+        //so total space complexity:- is O(n)
     }
 };
