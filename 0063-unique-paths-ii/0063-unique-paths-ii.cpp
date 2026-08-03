46a9bd66b02b682fb90fabca4a9a453f5c7e14d4
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-03 10:39:33 +0530

    Time: 1 ms (15.73%), Space: 11.9 MB (32.18%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -15,6 +15,7 @@ int helper(int i,int j,int m,int n,vector<vector<int>>&grid,vector<vector<int>>&
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1) return 0;
         vector<vector<int>>dp(m,vector<int>(n,-1));
         return helper(0,0,m,n,obstacleGrid,dp);
     }
