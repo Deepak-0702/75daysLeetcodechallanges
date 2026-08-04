499acef2194a822db01486d72aa615135c9efaac
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-04 15:49:12 +0530

    Time: 3 ms (43.69%), Space: 32.1 MB (94.22%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-bool solve(int st,vector<int>&arr,vector<bool>&visited){
-    int n=arr.size();
-    if(st<0 || st>=n){
-        return false;
-    }
-    if(visited[st]){
-        return false;
-    }
-    if(arr[st]==0){
-        return true;
-    }
-    visited[st]=true;
-    bool found=solve(st-arr[st],arr,visited)|| solve(st+arr[st],arr,visited);
+bool helper(vector<int>&arr,int st,vector<bool>vis){
+    if(st<0 || st>=arr.size()) return false;
+    if(vis[st]) return false;
+    if(arr[st]==0) return true;
+
+    vis[st]=true;
+    bool found=helper(arr,st+arr[st],vis) || helper(arr,st-arr[st],vis);
+
     return found;
+
 }
     bool canReach(vector<int>& arr, int start) {
-        int n=arr.size();
-        vector<bool>visited(n,false);
-        return solve(start,arr,visited);
+        vector<bool>vis(arr.size(),false);
+        return helper(arr,start,vis);
     }
 };
