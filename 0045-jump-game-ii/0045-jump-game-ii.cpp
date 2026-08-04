f56462a00c144d9301449775a4c5f0970939a84d
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-04 15:28:25 +0530

    Time: 0 ms (100%), Space: 20.7 MB (29.18%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-long long helper(int idx,vector<int>&nums,vector<long long>&dp){
-    if(idx>=nums.size()-1) return 0;
-    if(dp[idx]!=-1) return dp[idx];
-    long long mini=LLONG_MAX;
-    for(int i=1;i<=nums[idx];i++){
-        long long ans=helper(idx+i,nums,dp);
-        if(ans!=LLONG_MAX){
-            mini=min(mini,1+ans);
-        }
-    }
-    return dp[idx]=mini;
-}
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<long long>dp(n,-1);
-        int ans=helper(0,nums,dp);
-        return ans;
+        if(n==1) return 0;
+        int jumps=0;
+        int curEnd=0;
+        int farthest=0;
+        for(int i=0;i<n-1;i++){
+            farthest=max(farthest,i+nums[i]);
+
+            if(i==curEnd){
+                curEnd=farthest;
+                jumps++;
+            }
+        }
+        return jumps;
     }
 };
