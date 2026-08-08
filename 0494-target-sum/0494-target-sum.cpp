6e42c2274ff15fd09ae8160787eaafaa49aed1b9
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-08 14:19:45 +0530

    Time: 983 ms (5.08%), Space: 12.9 MB (62.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-long long helper(int idx,long long sum,int target,vector<int>&nums){
+long long helper(int idx,long long sum,int target,vector<int>&nums,
+vector<vector<long long>>&dp){
     if(idx==nums.size()){
         if(sum==target){
             return 1;
@@ -8,14 +9,16 @@ long long helper(int idx,long long sum,int target,vector<int>&nums){
         return 0;
     }
 
-    long long op1=helper(idx+1,sum+nums[idx],target,nums);
-    long long op2=helper(idx+1,sum-nums[idx],target,nums);
+    long long op1=helper(idx+1,sum+nums[idx],target,nums,dp);
+    long long op2=helper(idx+1,sum-nums[idx],target,nums,dp);
 
     return op1+op2;
 
 }
     int findTargetSumWays(vector<int>& nums, int target) {
-        long long  ways=helper(0,0,target,nums);
+        int n=nums.size();
+        vector<vector<long long>>dp(n,vector<long long>(n,-1));
+        long long  ways=helper(0,0,target,nums,dp);
         return (int)ways;
     }
 };
