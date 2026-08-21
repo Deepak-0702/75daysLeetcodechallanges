651abd2084f5eebf1b8dd500688204e37f6399b0
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-21 14:48:43 +0530

    Time: 4 ms (41.63%), Space: 55 MB (86.18%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-    
-        unordered_map<int,int>freq;
-        for(int num:nums){
-            if(num>=0){
-                freq[num]++;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            while(nums[i]>=1 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                swap(nums[nums[i]-1],nums[i]);
             }
         }
-        for(int i=1;i<=nums.size();i++){
-            if(freq[i]==0){
-                return i;
-                break;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
             }
         }
-        return nums.size()+1;
+        return n+1;
     }
 };
