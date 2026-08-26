e337dd6b5774f91e340f448a14418dc031d27c90
Deepak-0702 <deepakverma010105@gmail.com>
2026-08-26 12:14:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.53%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int n = s.size();
-        int mini = INT_MAX;
-        int j = 0;
-        int count = 0;
-        string ans = "";
-
-        for (int i = 0; i < n; i++) {
-
-            if (s[i] == '1') {
+        int n=s.size();
+        int j=0;
+        int count=0;
+        string ans="";
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
                 count++;
+                
             }
-
-            // exactly k ones
-            while (count == k) {
-
-                int len = i - j + 1;
-
-                if (len < mini) {
-                    mini = len;
-                    ans = s.substr(j, len);
+            while(count==k){
+                int len=i-j+1;
+                if(len<mini){
+                    mini=len;
+                    ans=s.substr(j,len);
                 }
-                else if (len == mini) {
-                    string curr = s.substr(j, len);
-
-                    if (curr < ans) {
-                        ans = curr;
+                else if(len==mini){
+                    string curr=s.substr(j,len);
+                    if(curr<ans){
+                        ans=curr;
                     }
                 }
-
-                // remove s[j]
-                if (s[j] == '1') {
+                if(s[j]=='1'){
                     count--;
                 }
-
                 j++;
             }
         }
-
         return ans;
+        
     }
 };
