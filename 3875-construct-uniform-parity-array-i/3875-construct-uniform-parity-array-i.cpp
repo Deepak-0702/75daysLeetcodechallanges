9838a5bc0246a41585de9ee74a58e21b18c312c7
Deepak-0702 <deepakverma010105@gmail.com>
2026-09-02 23:33:22 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        return true;
+        for(int num:nums1){
+            if(num%2==0 || num%2!=0){
+                return true;
+            }
+        }
+        return false;
     }
 };
