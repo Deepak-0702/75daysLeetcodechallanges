6173fdeed789c3db9726e584670e9688c916909b
Deepak-0702 <deepakverma010105@gmail.com>
2026-09-03 13:47:18 +0530

    Time: 0 ms (100%), Space: 30.4 MB (7.17%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,11 +1,45 @@
 class Solution {
 public:
+bool isodd(vector<int>&nums1){
+    bool flag=false;
+    for(int num:nums1){
+        if(num%2!=0){
+            flag=true;
+        }
+    }
+    if(flag){
+        return true;
+    }
+    return false;
+}
+bool iseven(vector<int>&nums1){
+    bool flag=false;
+    for(int num:nums1){
+        if(num%2==0){
+            flag=true;
+        }
+    }
+    if(flag){
+        return true;
+    }
+    return false;
+}
     bool uniformArray(vector<int>& nums1) {
+        int idx=0;
+        int ans=0;
         for(int num:nums1){
-            if(num%2==0 || num%2!=0){
-                return true;
+            if(num%2==0){
+                nums1[idx]=num;
+                ans=num;
+                idx++;
+            }
+            else{
+                nums1[idx]=num-ans;
+                ans=num;
+                idx++;
             }
         }
+        if(iseven(nums1) || isodd(nums1)) return true;
         return false;
     }
 };
